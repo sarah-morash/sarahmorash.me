318cd7968e8e643046d5b996b28339c4defecc43
Sarah Morash <sarahjmorash@gmail.com>
2020-04-26 18:03:18 -0300

    Hero naming

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import Layout from "../components/layout";
-import Hero from "../components/hero";
+import Hero from "../components/Hero";
 import SEO from "../components/seo";
 
 const IndexPage = () => {
